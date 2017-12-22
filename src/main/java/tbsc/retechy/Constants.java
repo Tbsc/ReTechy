5dbd1a91ba145608f1350372dea51618cd48200d
Tbsc <danielben60@gmail.com>
2017-12-22 22:21:03 +0200

    So apparently since I last developed mods they changed it so the modid must be lowercase

--- src/main/java/tbsc/retechy/Constants.java
+++ src/main/java/tbsc/retechy/Constants.java
@@ -7,7 +7,7 @@ package tbsc.retechy;
  */
 public class Constants {
 
-    public static final String MODID = "ReTechy";
+    public static final String MODID = "retechy";
     public static final String VERSION = "0.0.1";
 
 }
